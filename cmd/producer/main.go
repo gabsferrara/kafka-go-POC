fe37e79f557043d87b9d53001724a36ff626fc40
gabsferrara <gabrielbarbosa_ferrara@hotmail.com>
2023-09-27 16:02:28 -0300

    Recebimento SINCRONO

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
+	deliveryChan := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("messagem", "teste", producer, nil)
+	Publish("messagem", "teste", producer, nil, deliveryChan)
+	e := <-deliveryChan
+
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Erro ao enviar")
+	} else {
+		fmt.Println("Mesangem enviada: ", msg.TopicPartition)
+	}
+
 	producer.Flush(1000) // p dar tempo de enviar a mensagem
+
 }
 
 func NewKafkaProducer() *kafka.Producer {
@@ -23,13 +35,13 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
