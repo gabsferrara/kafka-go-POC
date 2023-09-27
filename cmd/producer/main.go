01cbdde1c6dd7c50c609d5ac5b36a1fa9ed2e235
gabsferrara <gabrielbarbosa_ferrara@hotmail.com>
2023-09-27 15:26:41 -0300

    first commit

---
+++ cmd/producer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	producer := NewKafkaProducer()
+	Publish("messagem", "teste", producer, nil)
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "kafka-poc-kafka-1:9092",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
